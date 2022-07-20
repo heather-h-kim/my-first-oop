df7876480431e0fbfef32c921fff6f394622a7a8
Heather Kim <heatherkim0405@gmail.com>
2022-07-20 16:16:18 -0500

    created a dropdown menu to select a guest

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
- console.log('in App');
+ const [guestList, setGuestList] = useState([]);
+ const [guestId, setGuestId] = useState('');
  const getGuests = () => {
   console.log('in getGuests');
    axios.get('/guests')
    .then((res) => {
     console.log('response.data is', res.data);
-    
+    setGuestList(res.data);
    }).catch((error) => {
     console.log('error getting guests');
     
@@ -20,10 +21,23 @@ function App() {
   getGuests();
  }, [])
 
+ console.log('guestList is', guestList);
+ 
+
   return (
     <div className="App">
       <h1> Message Generator </h1>
-      <h2> Guests </h2>
+      <label> Select Guest 
+      <select
+        value={guestId}
+        onChange={(event) => {
+          setGuestId(event.target.value);
+        }}>
+        {guestList.map(guest => (
+          <option key={guest.id} value={guest.id}>{guest.firstName} {guest.lastName}</option>
+        ))}
+      </select>
+      </label>
       <h2> Companies </h2>
       <h2> Templates </h2>
 
