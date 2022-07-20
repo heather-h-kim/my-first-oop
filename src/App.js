a4008e2ff16ec6befdcc97441767c69aa6639c7d
Heather Kim <heatherkim0405@gmail.com>
2022-07-20 15:49:56 -0500

    is able to get guests information from the server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+ console.log('in App');
+ const getGuests = () => {
+  console.log('in getGuests');
+   axios.get('/guests')
+   .then((res) => {
+    console.log('response.data is', res.data);
+    
+   }).catch((error) => {
+    console.log('error getting guests');
+    
+   })
+ }
+
+ useEffect(() => {
+  getGuests();
+ }, [])
+
   return (
     <div className="App">
       <h1> Message Generator </h1>
